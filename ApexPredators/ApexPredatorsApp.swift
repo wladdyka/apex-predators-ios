95fc9476a2b6a4ea159b7cad445a3f3018315642
Vlad M <wladdyka@gmail.com>
2024-07-01 07:35:04 +0500

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Vlad M on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApexPredators
+//
+//  Created by Vlad M on 01.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
