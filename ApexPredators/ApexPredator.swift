39d756b0ab9a94346c05c78b5c8bed6809803967
Vlad M <wladdyka@gmail.com>
2024-07-04 20:43:51 +0500

    initial commit

---
+++ ApexPredators/ApexPredator.swift
@@ -0,0 +1,25 @@
+//
+//  ApexPredator.swift
+//  ApexPredators
+//
+//  Created by Vlad M on 01.07.2024.
+//
+
+import Foundation
+
+struct ApexPredator: Decodable {
+    let id: Int
+    let name: String
+    let type: String
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    struct MovieScene: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
